c9d36c5bf80ca1eda7fb70a0f87970e3bbebbdb1
Giang <le.t1@edu.mirea.ru>
2025-12-06 15:17:28 +0300

    2nd commit

--- AoC2025/Program.cs
+++ AoC2025/Program.cs
@@ -1,10 +1,27 @@
-﻿int dial = 50;
+﻿using System.Security.AccessControl;
+
+int dial = 50;
 while (true)
 {
     
 }
 
-/*int Rotate(int dial, string command)
+int Rotate(int dial, string command)
 {
-    int
-}*/
+    int rotation;
+    switch (command[0])
+    {
+        case 'L':
+            rotation = Convert.ToInt32(command.Remove(0, 1));
+            if (dial - rotation < 0)
+                return 99 - rotation;
+            return 99 - rotation - dial;
+        case 'R':
+            rotation = Convert.ToInt32(command.Remove(0, 1));
+            if (dial + rotation > 99)
+                return 99 - rotation;
+            return dial + rotation;
+    }
+
+    return dial;
+}
